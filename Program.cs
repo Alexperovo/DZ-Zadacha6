b66738d709916242c3b19b821e693b3974e61907
Alexperovo <alexperovo@gmail.com>
2022-11-14 22:30:47 +0300

    работает

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// написать программу, которая на вход принимает число и выдает, является ли число чётным
+
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32 (Console.ReadLine ());
+if (num % 2 == 0) Console.WriteLine("Чётное");
+else Console.WriteLine ("Нечётное");
